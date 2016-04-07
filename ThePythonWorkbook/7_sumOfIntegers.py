f5268df53203b63641cb6299e8b1afe077842363
Stanislav Frolov <mail.stanislav.frolov@gmail.com>
2016-04-07 22:08:49 +0200

    test

--- ThePythonWorkbook/7_sumOfIntegers.py
+++ ThePythonWorkbook/7_sumOfIntegers.py
@@ -2,6 +2,9 @@
 # Write a program that reads a positive integer, n, from the user and then displays the
 # sum of all of the integers from 1 to n. The sum of the first n positive integers can be
 # computed using the formula: sum = ((n)n+1)/2
+
+# test github
+
 import unittest
 
 x = 7
