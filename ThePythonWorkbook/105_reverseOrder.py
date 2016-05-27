ff192ca176922202cace7fa310bb2edccf5cd0ae
stanislavfrolov92 <mail.stanislav.frolov@gmail.com>
2016-05-27 21:03:41 +0200

    done #105

--- ThePythonWorkbook/105_reverseOrder.py
+++ ThePythonWorkbook/105_reverseOrder.py
@@ -5,7 +5,10 @@ def main():
     number = int(input("give number - 0 for end: "))
     listOfNumbers.append(number)
   listOfNumbers.remove(0)
-  print(sorted(listOfNumbers))
+  listOfNumbers = sorted(listOfNumbers)
+  listOfNumbers = reversed(listOfNumbers)
+  for element in listOfNumbers:
+    print(element)
 
 if __name__ == '__main__':
     main()
