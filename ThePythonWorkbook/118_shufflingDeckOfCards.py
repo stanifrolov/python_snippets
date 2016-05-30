1f565e0e405c5eba8a5897ade28ca8c3c0b7363b
Stanislav Frolov <mail.stanislav.frolov@gmail.com>
2016-05-30 21:10:15 +0200

    #118

--- ThePythonWorkbook/118_shufflingDeckOfCards.py
+++ ThePythonWorkbook/118_shufflingDeckOfCards.py
@@ -1,5 +1,32 @@
+import unittest
+import random
+
 def main():
-  pass
+  print(createDeck())
+  print(shuffle(createDeck()))
+
+def shuffle(listOfElements):
+    for element in listOfElements:
+        randomIndexOne = random.randint(0, 51)
+        randomIndexTwo = random.randint(0, 51)
+        temp = listOfElements[randomIndexOne]
+        listOfElements[randomIndexOne] = listOfElements[randomIndexTwo]
+        listOfElements[randomIndexTwo] = temp
+    return listOfElements
+
+def createDeck():
+    listOfCards = []
+    cardValues = [1, 2, 3, 4, 5, 6, 8, 9, 'T', 'J', 'Q', 'K', 'A']
+    cardSuits = ['s', 'h', 'd', 'c']
+    for valueOfCard in cardValues:
+        for cardSuit in cardSuits:
+            listOfCards.append(str(valueOfCard) + cardSuit)
+    return listOfCards
+
+class UnitTests(unittest.TestCase):
+
+    def testSizeOfCreatedCardList(self):
+        self.assertEqual(len(createDeck()), 52)
 
 if __name__ == '__main__':
     main()
